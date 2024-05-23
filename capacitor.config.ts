5bf7fa16de9ac1a6def7bf8610de6a5132b4f15b
brice512 <beckhambrice1@gmail.com>
2024-05-23 09:21:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tasks',
+  appName: 'tasks',
+  webDir: 'www'
+};
+
+export default config;
